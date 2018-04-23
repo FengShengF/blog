a62ab1113fa554c603e5f0ecaa0e2382f798091d
FengShengF <1145485505@qq.com>
2018-04-23 13:39:38 +0800

    s

--- blog_back/src/components/main/addarticle.vue
+++ blog_back/src/components/main/addarticle.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="article">
     <h3>添加文章</h3>
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+    <el-form :model="ruleForm2" status-icon :rules="rules" ref="ruleForm2" label-width="100px" class="demo-ruleForm2">
       <el-form-item label="文章标题" prop="cnname_one" :rules="[{required:true,message:'不能为空'}]">
         <el-input type="text" auto-complete="off"></el-input>
       </el-form-item>
       <el-form-item label="一级类名" prop="cnname_one" :rules="[{required:true,message:'不能为空'}]">
         <el-select placeholder='请选择'>
-          <el-option>
+          <el-option v-for='item in options' :key="item.id" :label='item.cnname' :value='item.id'>
           </el-option>
         </el-select>
       </el-form-item>
@@ -36,10 +36,10 @@
       </p>
       <el-form-item label="时间" prop="cnname_one">
         <el-time-select v-model="value1" :picker-options="{
-              start: '08:30',
-              step: '00:15',
-              end: '18:30'
-            }" placeholder="选择时间">
+          start: '08:30',
+          step: '00:15',
+          end: '18:30'
+        }" placeholder="选择时间">
         </el-time-select>
       </el-form-item>
       <el-form-item label="缩略图" prop="cnname_one">
@@ -51,6 +51,10 @@
           <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
         </el-upload>
       </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+        <el-button @click="resetForm('ruleForm2')">重置</el-button>
+      </el-form-item>
     </el-form>
   </div>
 </template>
@@ -60,11 +64,47 @@ export default {
     return {
       radio: '1',
       textarea: '',
-      value1: ''
+      value1: '',
+      ruleForm2: {
+        enname_two: '',
+        cnname_two: '',
+        oneId: ''
+      },
+      options: []
     };
   },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.axios.post('/api/back/class/getOneClass', this.ruleForm2).then((data) => {
+            console.log(data)
+            if (data.data.code == '2021') {
+              this.$message({
+                showClose: true,
+                message: '恭喜你,提交成功',
+                type: 'success'
+              })
+            } else {
+              this.$message({
+                showClose: true,
+                message: '获取失败',
+                type: 'error'
+              })
+            }
+          })
+        } else {
+          console.log('error submit!')
+          return false;
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  },
   created() {
-      this.axios.get('/api/back/class/wenzhang').then((data)=>{
+      this.axios.get('/api/back/class/getOneClass').then((data)=>{
           this.options=data.data.data
           console.log(data.data.data)
       })
--- blog_back/src/components/main/amend_class.vue
+++ blog_back/src/components/main/amend_class.vue
@@ -1,101 +1,92 @@
 <template>
-    <div class="classmain">
-        <el-form :model='ruleForm' :rules='rules' ref="ruleFrom" label-width='100%' class="demo-ruleForm">
-            <template v-if='state'>
-                <p>一级分类列表</p>
-                <el-form-item label='一级中文名' props='cnname'>
-                    <el-input type='text' v-model="ruleForm.cnname" auto-complete='off'></el-input>
-                </el-form-item>
-                <el-form-item label='一级英文名' props='enname'>
-                    <el-input type='text' v-model="ruleForm.enname" auto-complete='off'></el-input>
-                </el-form-item>
-            </template>
-            <template v-else>
-                <p>二级分类列表</p>
-                <el-form-item label='二级中文名' props='cnname'>
-                    <el-input type='text' v-model="ruleForm.cnname" auto-complete='off'></el-input>
-                </el-form-item>
-                <el-form-item label='二级英文名' props='enname'>
-                    <el-input type='text' v-model="ruleForm.enname" auto-complete='off'></el-input>
-                </el-form-item>
-            </template>
+    <div>
+        <el-form v-if="state" status-icon label-width="100px" class="demo-ruleForm">
+            <h3>一级分类列表</h3>
+            <el-form-item label="一级中文类名">
+                <el-input style="width:200px" v-model="ruleForm.enname"></el-input>
+            </el-form-item>
+            <el-form-item label="一级英文类名">
+                <el-input style="width:200px" v-model="ruleForm.cnname"></el-input>
+            </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
+                <template>
+                    <el-button size="mini" @click="handleEdit">编辑</el-button>
+                    <el-button size="mini" type="danger">删除</el-button>
+                </template>
+            </el-form-item>
+        </el-form>
+        <el-form v-else status-icon label-width="100px" class="demo-ruleForm">
+            <h3>二级分类列表</h3>
+            <el-form-item label="二级中文类名">
+                <el-input style="width:200px" v-model="ruleForm.enname"></el-input>
+            </el-form-item>
+            <el-form-item label="二级英文类名">
+                <el-input style="width:200px" v-model="ruleForm.cnname"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <template>
+                    <el-button size="mini" @click="handleEdit">编辑</el-button>
+                    <el-button size="mini" type="danger">删除</el-button>
+                </template>
             </el-form-item>
         </el-form>
     </div>
 </template>
+
 <script>
 export default {
     data() {
-        var calidatePass = (rule, value, callback) => {
-            if (value === '') {
-                callback(new Error('请输入英文姓名'))
-            } else {
-                if (/^[a-zA-Z]{4,12}$/.test(value)) {
-                    callback();
-                } else {
-                    callback(new Error('请输入正确的英文名'))
-                }
+        return {
+            cnname: "",
+            enname: "",
+            state: true,
+            oldenname: "",
+            ruleForm: {
+
             }
         };
-        return {
-            ruleForm: {
-                enname_one: '',
-                cnname_one: '',
-                oldenname_one: ''
-            },
-            rules: {
-                enname: [
-                    { validator: validatePass, trigger: 'blur' }
-                ]
-            },
-            state: true
-        }
-    },
-    methods: {
-        submitForm(formName) {
-            this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    // this.axios.post('/api/back/class/amendClassOne', this.ruleForm2).then((data) => {
-                    //     console.log(data)
-                    //     if (data.data.code == '1137') {
-                    //         this.$message({
-                    //             showClose: true,
-                    //             message: data.data.msg,
-                    //             type: 'success'
-                    //         })
-                    //     } else {
-                    //         this.$message({
-                    //             showClose: true,
-                    //             message: data.data.msg,
-                    //             type: 'error'
-                    //         })
-                    //     }
-                    // })
-                } else {
-                    console.log('error submit!')
-                    return false;
-                }
-            })
-        },
-        resetForm(formName) {
-            this.$refs[formName].resetFields();
-        }
     },
     mounted() {
-        let state = this.$route.query.state
-        if (state == 'one') {
+        let state = this.$route.query.state;
+        //one判断是一级还是二级
+        if (state == "one") {
             this.state = true;
-            const data = JSON.parse(sessionStorage.getItem('oneClassData'))
+            const data = JSON.parse(sessionStorage.getItem("info"));
             Object.entries(data).forEach(function(i) {
                 this.$set(this.ruleForm, i[0], i[1])
             }, this)
+            this.ruleForm.id = data.id;
+        } else {
+            this.state = false;
+            const data = JSON.parse(sessionStorage.getItem("info"));
+            Object.entries(data).forEach(function(i) {
+                this.$set(this.ruleForm, i[0], i[1])
+            }, this)
+            this.ruleForm.id = data.id;
         }
-
-        // console.log(this.ruleForm)
-        // console.log(Object.getOwnPropertyDescriptor(this.ruleForm, 'enname'))
+    },
+    methods: {
+        handleEdit() {
+            let state = this.$route.query.state;
+            if (state == "one") {
+                this.axios
+                .post("/api/back/class/amendClass", this.ruleForm)
+                .then(data => {
+                    console.log(data);
+                });
+            } else {
+                this.state = false;
+                this.axios
+                .post("/api/back/class/amendClassTwo", this.ruleForm)
+                .then(data => {
+                    console.log(data);
+                });
+            }
+        }
     }
-}
+};
 </script>
+
+<style>
+
+</style>
