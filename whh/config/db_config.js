67f35dd051e4ff53203b13961f6dcc5a26cb40e8
huan1018 <362840264@qq.com>
2018-04-24 12:22:33 +0800

    whh

--- whh/config/db_config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    dev_db: {
-        host: "39.105.11.231",
-        user: "root",
-        password: "root",
-        port: 3306,
-        database: "blog_sql"
-    }
-}
--- whhhh/src/components/main/apilist.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>接口列表</div>
-</template>
